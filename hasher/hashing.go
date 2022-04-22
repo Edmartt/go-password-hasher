a5a56567696d4f56b3896633f580d2643df61f70
Edmartt <47486245+Edmartt@users.noreply.github.com>
2022-04-21 23:17:10 -0500

    Added new doc comments

--- hasher/hashing.go
+++ hasher/hashing.go
@@ -1,10 +1,11 @@
+//This package provide a function to convert plain text to hash
 package hasher
 
 import (
 	"golang.org/x/crypto/bcrypt" //For generating random salted hash from text plain
 )
 
-//This function takes a string and converts it to hash with pbdkf2 and sha256
+//This function takes a string and converts it to hash with pbdkf2 and sha256 based on bcrypt package
 func ConvertToHash(password string) string {
 	
 	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
