4a3d0712fa88a919500785c3346f3ae2c59660a7
Edmartt <47486245+Edmartt@users.noreply.github.com>
2022-04-22 07:47:51 -0500

    Added log instead of panic
    Now this function logs in case of error

--- hasher/hashing.go
+++ hasher/hashing.go
@@ -2,15 +2,19 @@
 package hasher
 
 import (
+	"log"
+
 	"golang.org/x/crypto/bcrypt" //For generating random salted hash from text plain
 )
 
 //This function takes a string and converts it to hash with pbdkf2 and sha256 based on bcrypt package
 func ConvertToHash(password string) string {
 	
-	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	return string(hashed_password)
+
+	return string(hashedPassword)
 }
